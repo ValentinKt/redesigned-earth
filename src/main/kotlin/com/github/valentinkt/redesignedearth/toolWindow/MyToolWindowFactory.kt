af5c43df357b9d10c8282609c2a4d8367bdd390c
GitHub Action <action@github.com>
2023-08-24 21:29:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/valentinkt/redesignedearth/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/valentinkt/redesignedearth/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.valentinkt.redesignedearth.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.valentinkt.redesignedearth.MyBundle
+import com.github.valentinkt.redesignedearth.services.MyProjectService
 import javax.swing.JButton
 
 
